ac5946b780c4d3d5262823eab758e74acff6a9b3
AyoubBoudhafr <a23mohbounij@inspedralbes.cat>
2025-03-13 16:26:59 +0100

    cambios para el build

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true, 
-  output: "export",
+  output: "standalone",
   images: {
-    unoptimized: true // Necesario si usas `next/image`
+    unoptimized: true
   }
 };
 
